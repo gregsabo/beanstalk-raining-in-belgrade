db33cee11208cfaba6844204aad5c812a605eb8c
Greg Sabo <gregsabo@stripe.com>
2016-01-25 08:14:10 -0800

    Display a random quote

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import Helmet from 'react-helmet';
 import connectData from 'helpers/connectData';
-import { isLoaded, load } from 'redux/modules/raining';
+import { isLoaded, load, chooseQuote } from 'redux/modules/raining';
 import { connect } from 'react-redux';
 
 function fetchData(getState, dispatch) {
+  return dispatch(chooseQuote());
+}
+
+function fetchDataDeferred(getState, dispatch) {
   if (!isLoaded(getState())) {
     return dispatch(load());
   }
@@ -17,19 +21,24 @@ function renderRaining(isRaining) {
   return <h1>The skies are clear in Belgrade.</h1>;
 }
 
-@connectData(null, fetchData)
+@connectData(fetchData, fetchDataDeferred)
 @connect(state => ({
-  isRaining: state.raining.get('isRaining')
+  isRaining: state.raining.get('isRaining'),
+  quote: state.raining.get('quote')
 }))
 export default class Home extends Component {
   static propTypes = {
-    isRaining: PropTypes.bool
+    isRaining: PropTypes.bool,
+    quote: PropTypes.string
   }
   render() {
     return (
       <div>
         <Helmet title="Home"/>
         {renderRaining(this.props.isRaining)}
+        <blockquote>
+          {this.props.quote}
+        </blockquote>
       </div>
     );
   }
--- src/redux/modules/raining.js
+++ src/redux/modules/raining.js
@@ -3,16 +3,24 @@ import { Map } from 'immutable';
 import actionTrio from 'utils/actionTrio';
 
 const LOAD = actionTrio('raining-in-belgrade/raining/LOAD');
+const QUOTE_CHOSEN = 'raining-in-belgrade/raining/QUOTE_CHOSEN';
 
 const initialState = {
-  isRaining: null
+  isRaining: null,
+  quote: null
 };
+
+function choose(inList) {
+  return inList[Math.floor(Math.random() * inList.length)];
+}
 
 export default function reducer(stateObj = initialState, action = {}) {
   const state = new Map(stateObj);
   switch (action.type) {
     case LOAD.SUCCESS:
       return state.set('isRaining', action.result.isRaining);
+    case QUOTE_CHOSEN:
+      return state.set('quote', action.payload);
     default:
       return state;
   }
@@ -26,5 +34,23 @@ export function load() {
   return {
     types: LOAD.trio,
     promise: client => client.get('/raining')
+  };
+}
+
+const QUOTES = [
+  "One shouldn't be afraid of the humans. Well, I am not afraid of the humans, but of what is inhuman in them.",
+  'If people would know how little brain is ruling the world, they would die of fear.',
+  "What doesn't hurt - is not life; what doesn't pass - is not happiness.",
+  'Forgetfulness heals everything and song is the most beautiful manner of forgetting, for in song man feels only what he loves.',
+  'What does your sorrow do while you sleep? -It’s awake and waiting. And when it loses patience, it wakes me up.',
+  'Lands of great discoveries are also lands of great injustices.',
+  'beautiful soul weeps deep',
+  'I do not fear invisible worlds.',
+];
+
+export function chooseQuote() {
+  return {
+    type: QUOTE_CHOSEN,
+    payload: choose(QUOTES)
   };
 }
